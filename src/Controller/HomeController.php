f1cd49a46ebb8182c0aedfe585eb93369580a678
enael.weber@ecole-hexagone.com <enael.weber@ecole-hexagone.com>
2026-01-13 12:52:50 +0100

    Fix TP2

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -6,9 +6,31 @@ use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HomeController extends AbstractController {
-	#[Route('/', name: 'app_home')]
+	#[Route('/', name: 'index')]
 	public function index(): Response {
 		return $this->render("home/home.html.twig");
+	}
+
+	#[Route('/random', name: 'random')]
+	public function random(): Response {
+		$quotes = [
+			'Le code est poésie.',
+			'Symfony simplifie la complexité.',
+			'Toujours tester, jamais supposer.',
+			'Refactoriser, c’est aimer son futur soi.'
+		];
+		
+		$quote = $quotes[random_int(0, sizeof($quotes)-1)];
+		return $this->render("home/random.html.twig", ["quote" => $quote]);
+	}
+
+	#[Route('/about', name: 'about')]
+	public function about(): Response {
+		return $this->render("home/about.html.twig");
+	}
+
+	#[Route('/hello/{name}', name: 'hello')]
+	public function hello(string $name): Response {
 	}
 }
 
